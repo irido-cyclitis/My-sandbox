8c59e618926df8a5350e42c4deea0508a1af2f42
Morad Abdelrasheed Mokhtar Ali Gill <48622891+irido-cyclitis@users.noreply.github.com>
2020-12-29 11:03:02 +0200

    Create HW.java

---
+++ HW.java
@@ -0,0 +1,41 @@
+
+public class HW {
+
+	public static void main(String[] args) {
+		
+		// init
+		
+		double out = 0;
+		double[] values = {5,6,7,8,9};
+		
+		// Mean
+		
+		double mean = 0;
+		for(int i = 0;i<values.length;i++) {
+			mean = mean + values[i];
+		}
+		mean = mean / values.length;
+		
+		// Numerator
+		
+		for(int i = 0;i<values.length;i++) {
+			values[i] = Math.pow(values[i] - mean, 2);
+		}
+		double numerator = 0;
+		for(int i = 0;i<values.length;i++) {
+			numerator = numerator + values[i];
+		}
+		
+		// Calculation
+		
+		out = Math.sqrt(
+				numerator / values.length
+				);
+		
+		// Print
+		
+		System.out.println("Standard Deviation : "+out+" = " + ("sqrt("+numerator+"/"+values.length+")")
+				+"\n"+"Mean : "+mean);
+	}
+
+}
